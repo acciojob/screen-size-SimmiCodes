070a3087f9d0c1349d647517327d75df22ed3568
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-10-25 21:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+
+function getWindowSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  return { width, height };
+}
+
+
+function updateWindowSize() {
+  const windowSize = getWindowSize();
+  document.getElementById("width").textContent = `Width: ${windowSize.width}px`;
+  document.getElementById("height").textContent = `Height: ${windowSize.height}px`;
+}
+
+
+window.addEventListener("resize", updateWindowSize);
+
+
+updateWindowSize();
